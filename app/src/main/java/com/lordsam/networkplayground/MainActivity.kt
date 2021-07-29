fda3ba0cf52a99e62b94ffb221bff83337cc77c2
LordSam <49715934+LordSam07@users.noreply.github.com>
2021-07-30 00:25:37 +0530

    Initial commit

---
+++ app/src/main/java/com/lordsam/networkplayground/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.lordsam.networkplayground
+
+import android.os.Bundle
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var telephonyManager: TelephonyManager? = null
+    var psListener: MyPhoneStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        psListener = MyPhoneStateListener()
+        telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        telephonyManager!!.listen(psListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS)
+    }
+}
---
+++ app/src/main/java/com/lordsam/networkplayground/MyPhoneStateListener.kt
@@ -0,0 +1,21 @@
+package com.lordsam.networkplayground
+
+import android.telephony.PhoneStateListener
+import android.telephony.SignalStrength
+import android.util.Log
+
+
+class MyPhoneStateListener : PhoneStateListener() {
+
+    private var signalStrengthValue = 0
+
+    override fun onSignalStrengthsChanged(signalStrength: SignalStrength){
+        super.onSignalStrengthsChanged(signalStrength)
+        signalStrengthValue = if (signalStrength.isGsm) {
+            if (signalStrength.gsmSignalStrength != 99) signalStrength.gsmSignalStrength * 2 - 113 else signalStrength.gsmSignalStrength
+        } else {
+            signalStrength.cdmaDbm
+        }
+        Log.i("strength", signalStrengthValue.toString())
+    }
+}
